550d817988655afe9ee0b4af917743a4b1b19935
Next Developer <information.instaforex@gmail.com>
2025-10-19 15:27:02 +0600

    deploy

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,15 +1,5 @@
 import { allNav } from "./allNav";
 
-/* export const getNavs = (role) => {
-  return allNav.filter(nav => nav.role === role);
-}; */
 export const getNavs = (role) => {
-    const finalNavs = [];
-
-    for (let i = 0; i < allNav.length; i++) {
-        if (role === allNav[i].role) {
-            finalNavs.push(allNav[i])
-        }
-    }
-    return finalNavs
+  return allNav.filter(nav => nav.role === role);
 };
